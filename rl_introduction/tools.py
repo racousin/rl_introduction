0dbc222db954454d8d022da0e3eb629dc1171efb
racousin <racousin@student.42.fr>
2023-01-22 15:09:02 +0100

    Revert "upgrade gym"
    This reverts commit 396b7e8b785fe112dc0921023391180a3ae54452.
    
    	modified:   notebooks/1_Environment_Agent.ipynb
    	modified:   notebooks/2_Dynamic_Programming.ipynb
    	modified:   notebooks/3_Temporal_Difference.ipynb
    	modified:   notebooks/4_deep_Q-learning.ipynb
    	modified:   notebooks/5_policy_gradient-reinforce.ipynb
    	modified:   notebooks/6_RL_framework.ipynb
    	modified:   rl_introduction/tools.py

--- rl_introduction/tools.py
+++ rl_introduction/tools.py
@@ -1,55 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import gymnasium as gym
+import gym
 import scipy.signal
 
-
 class Agent:
-    def __init__(self, env, gamma=0.99, epsilon=0.01):
+    def __init__(self, env, gamma = .99, epsilon = .01):
         self.env = env
-        self.policy = (
-            np.ones([self.env.observation_space.n, self.env.action_space.n])
-            / self.env.action_space.n
-        )
+        self.policy = np.ones([self.env.observation_space.n, self.env.action_space.n]) / self.env.action_space.n
         self.gamma = gamma
         self.epsilon = epsilon
-
     def act(self, state):
-        action = np.random.choice(
-            np.arange(self.env.action_space.n), p=self.policy[state]
-        )
+        action = np.random.choice(np.arange(self.env.action_space.n),p=self.policy[state])
         return action
-
     def train(self, current_state, action, reward, state, done):
         pass
-
 
 class MyRandomAgent(Agent):
     def __init__(self, env):
         super().__init__(env)
-        self.policy = (
-            np.ones([self.env.observation_space.n, self.env.action_space.n])
-            / self.env.action_space.n
-        )
-
+        self.policy = np.ones([self.env.observation_space.n, self.env.action_space.n]) / self.env.action_space.n
     def act(self, state):
-        action = np.random.choice(
-            np.arange(self.env.action_space.n), p=self.policy[state]
-        )
+        action = np.random.choice(np.arange(self.env.action_space.n),p=self.policy[state])
         return action
 
-
 class DeepAgent:
-    def __init__(self, env, gamma=0.99, epsilon=0.01):
+    def __init__(self, env, gamma = .99, epsilon = .01):
         self.env = env
         self.gamma = gamma
         self.epsilon = epsilon
         self.state_dim = env.observation_space.shape[0]
         self.action_dim = env.action_space.n
-
     def act(self, state):
         pass
-
     def train(current_state, action, reward, done):
         pass
 
@@ -63,27 +45,27 @@ def policy_evaluation(env, policy, gamma=1, theta=1e-8):
             for a, action_prob in enumerate(policy[s]):
                 for prob, next_state, reward, done in env.P[s][a]:
                     Vs += action_prob * prob * (reward + gamma * V[next_state])
-            delta = max(delta, np.abs(V[s] - Vs))
+            delta = max(delta, np.abs(V[s]-Vs))
             V[s] = Vs
         if delta < theta:
             break
     return V
 
 
+
 def plot_values_lake(V):
     # reshape value function
-    V_sq = np.reshape(V, (4, 4))
+    V_sq = np.reshape(V, (4,4))
 
     # plot the state-value function
     fig = plt.figure(figsize=(6, 6))
     ax = fig.add_subplot(111)
-    im = ax.imshow(V_sq, cmap="cool")
-    for (j, i), label in np.ndenumerate(V_sq):
-        ax.text(i, j, np.round(label, 5), ha="center", va="center", fontsize=14)
+    im = ax.imshow(V_sq, cmap='cool')
+    for (j,i),label in np.ndenumerate(V_sq):
+        ax.text(i, j, np.round(label, 5), ha='center', va='center', fontsize=14)
     plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
-    plt.title("State-Value Function")
+    plt.title('State-Value Function')
     plt.show()
-
 
 def q_from_v(env, V, s, gamma=1):
     q = np.zeros(env.action_space.n)
@@ -92,38 +74,30 @@ def q_from_v(env, V, s, gamma=1):
             q[a] += prob * (reward + gamma * V[next_state])
     return q
 
-
 def policy_improvement(env, V, gamma=1):
-    policy = (
-        np.zeros([env.observation_space.n, env.action_space.n]) / env.action_space.n
-    )
+    policy = np.zeros([env.observation_space.n, env.action_space.n]) / env.action_space.n
     for s in range(env.observation_space.n):
         q = q_from_v(env, V, s, gamma)
-        best_a = np.argwhere(q == np.max(q)).flatten()
-        policy[s] = np.sum(
-            [np.eye(env.action_space.n)[i] for i in best_a], axis=0
-        ) / len(best_a)
+        best_a = np.argwhere(q==np.max(q)).flatten()
+        policy[s] = np.sum([np.eye(env.action_space.n)[i] for i in best_a], axis=0)/len(best_a)
 
     return policy
 
-
-def value_iteration(env, gamma=1, theta=1e-8):
+def value_iteration(env, gamma=1, theta=1e-8):  
     V = np.zeros(env.observation_space.n)
     while True:
         delta = 0
         for s in range(env.observation_space.n):
             v = V[s]
             V[s] = max(q_from_v(env, V, s, gamma))
-            delta = max(delta, abs(V[s] - v))
+            delta = max(delta,abs(V[s]-v))
         if delta < theta:
             break
     policy = policy_improvement(env, V, gamma)
     return policy, V
 
-
 def discount_cumsum(x, discount):
     return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
-
 
 def run_experiment_episode_train(env, agent, nb_episode):
     rewards = np.zeros(nb_episode)
@@ -134,9 +108,9 @@ def run_experiment_episode_train(env, agent, nb_episode):
         while done is False:
             action = agent.act(state)
             current_state = state
-            state, reward, terminated, truncated, info = env.step(action)
+            state, reward, done, info = env.step(action)
             agent.train(current_state, action, reward, state, done)
             rews.append(reward)
         rewards[i] = sum(rews)
-        print("episode: {} - cum reward {}".format(i, rewards[i]))
+        print('episode: {} - cum reward {}'.format(i, rewards[i]))
     return rewards
