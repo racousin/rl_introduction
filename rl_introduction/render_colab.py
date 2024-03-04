a19b9c74c6761c74ec7e8e5ade2d2c09e3e21df3
racousin <racousin@student.42.fr>
2024-03-04 19:55:23 +0100

    update tools

--- rl_introduction/render_colab.py
+++ rl_introduction/render_colab.py
@@ -1,118 +1,66 @@
-from IPython.core.display import Video, display
-from colabgymrender.recorder import Recorder
-from moviepy.editor import *
-import time
+import gymnasium as gym
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+from IPython import display as ipythondisplay
+from pyvirtualdisplay import Display
 import cv2
-import gym
-import os
 
-os.environ["SDL_VIDEODRIVER"] = "dummy"
+virtual_display = Display(visible=0, size=(1400, 900))
+virtual_display.start()
 
+# Function to save frames and display as video
+def save_video_of_model(exp_name, env_name, frames, fps, scale=2):
+    path = f"{exp_name}.mp4"
+    writer = imageio.get_writer(path, fps=fps)
+    for frame in frames:
+        # Resize frame if environment is Pong or FrozenLake
+        if env_name in ['PongNoFrameskip-v4', 'FrozenLake-v1']:
+            frame = cv2.resize(frame, None, fx=scale, fy=scale, interpolation=cv2.INTER_NEAREST)
+        writer.append_data(frame)
+    writer.close()
+    return path
 
-class Recorder(gym.Wrapper):
-    def __init__(
-        self,
-        env,
-        directory,
-        auto_release=True,
-        size=None,
-        fps=None,
-        rgb=True,
-        slow_coeff=1,
-    ):
-        super(Recorder, self).__init__(env)
-        self.directory = directory
-        self.auto_release = auto_release
-        self.active = True
-        self.rgb = rgb
-        self.slow_coeff = slow_coeff
-
-        if not os.path.exists(self.directory):
-            os.mkdir(self.directory)
-
-        if size is None:
-            self.env.reset()
-            self.size = self.env.render(mode="rgb_array").shape[:2][::-1]
-        else:
-            self.size = size
-
-        if fps is None:
-            if "video.frames_per_second" in self.env.metadata:
-                self.fps = self.env.metadata["video.frames_per_second"]
-            else:
-                self.fps = 30
-        else:
-            self.fps = fps
-
-    def pause(self):
-        self.active = False
-
-    def resume(self):
-        self.active = True
-
-    def _start(self):
-        self.cliptime = time.time()
-        self.path = f"{self.directory}/{self.cliptime}.mp4"
-        fourcc = cv2.VideoWriter_fourcc(*"MP4V")
-        self._writer = cv2.VideoWriter(self.path, fourcc, self.fps, self.size)
-
-    def _write(self):
-        if self.active:
-            frame = self.env.render(mode="rgb_array")
-            if self.rgb:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            for i in range(self.slow_coeff):
-                self._writer.write(frame)
-
-    def release(self):
-        self._writer.release()
-
-    def reset(self, *args, **kwargs):
-        observation = self.env.reset(*args, **kwargs)
-        self._start()
-        self._write()
-        return observation
-
-    def step(self, *args, **kwargs):
-        data = self.env.step(*args, **kwargs)
-        self._write()
-
-        if self.auto_release and data[2]:
-            self.release()
-
-        return data
-
-    def play(self):
-        start = time.time()
-        filename = "temp-{start}.mp4"
-        clip = VideoFileClip(self.path)
-        clip.write_videofile(filename, progress_bar=False, verbose=False)
-        display(Video(filename, embed=True))
-        os.remove(filename)
+# Function to display video
+def display_video(path):
+    ipythondisplay.display(ipythondisplay.Video(path, embed=True))
 
 
-def gym_render(
-    env_name="FrozenLake-v1",
-    env=None,
-    directory="./video",
-    size=None,
-    fps=None,
-    slow_coeff=2,
-    agent="random",
-    max_step=500,
-):
-    if env is None:
-        env = gym.make(env_name)
-    directory = directory
-    env = Recorder(env, directory, slow_coeff=slow_coeff, size=size, fps=fps)
-    state = env.reset()
+
+
+def exp_render(env_config):
+    virtual_display = Display(visible=0, size=(1400, 900))
+    virtual_display.start()
+    # Setup the virtual display
+    virtual_display = Display(visible=0, size=(1400, 900))
+    virtual_display.start()
+    env = gym.make(env_config["name"], render_mode='rgb_array')
+    observation = env.reset()
     done = False
-    while not done or max_step > 0:
-        if agent == "random":
-            action = env.action_space.sample()
-        else:
-            action = agent.act(state)
-        state, reward, done, info = env.step(action)
-        max_step -= 1
+    step = 0
+    frames = []
 
-    return env.play()
+
+    while step < env_config["nb_step"]:  # Limit steps to avoid long loops
+        if "agent" in env_config:
+            action = env_config["agent"].act(observation)
+        else:
+            action = env.action_space.sample()
+        observation, reward, done, info, _ = env.step(action)
+        step += 1
+        if done:
+          rend = env.render()
+          frames += [rend] * env_config["fps"]
+          observation = env.reset()
+          done = False
+
+
+        frames.append(env.render())
+
+
+    env.close()
+    
+
+    video_path = save_video_of_model(env_config["name"], env_config["name"], frames, env_config["fps"])
+    display_video(video_path)
+    virtual_display.stop()
